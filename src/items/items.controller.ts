08d27476ace8e50fc8d17c30aa79d0ef95040c25
Shreya Kaushik <shreyakaushik11@gmail.com>
2020-07-28 11:47:13 +0530

    created a dto file for create items and implemented the create request using post'

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { CreateItemDto } from './dto/create-item.dto';
+import { Controller, Get, Post, Put, Delete, Body } from '@nestjs/common';
 
 @Controller('items')
 export class ItemsController {
     @Get()
-    findAll():string{
+    findAll(): string{
         return 'get all items';
+    }
+
+    @Post()
+    create(@Body() createItemDto: CreateItemDto): string{
+        return `Name: ${createItemDto.name} Desc: ${createItemDto.description}`;
     }
 }
